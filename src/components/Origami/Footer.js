329b45a0492c724f9289421baed461c74d57a9f3
Dhammika-Mahendra <wmdhammikamahendra@gmail.com>
2024-07-12 18:59:13 +0530

    Origami - distructured in componentvise

---
+++ src/components/Origami/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../Origami/Footer.css'
+
+export default function Footer() {
+  return (
+    <div id='footTriangle'>
+
+    </div>
+  )
+}
--- src/components/Origami/Header.js
+++ src/components/Origami/Header.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import '../Origami/Header.css'
 
 export default function Header() {
   return (
-    <div>
-        
+    <div style={{position:'fixed',top:'0',left:'850px'}}>{/* --- to give whole unit a fixed behavior */}
+      <div style={{position:'relative'}}>{/* --- to control elements as absolute */}
+            <div id='navBarFall'></div>{/* --- triangle falling off the navbar ------- */}
+            <div className='drop'>{/* --- Navbar menu ------- */}
+              <div id='navBar'></div>
+            </div>
+      </div>
     </div>
   )
 }
